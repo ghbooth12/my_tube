03e9fe63776e0e70a616acbfeabc0dcb357bb825
Gahee Heo <gahee@studentloangenius.com>
2018-03-04 11:26:19 -0600

    video detail

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAQWwQB2gg13bzsaIWvTRaHqoc8yvE_RFM';
 
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={ this.state.videos[0] } />
         <VideoList videos={ this.state.videos } />
       </div>
     );
